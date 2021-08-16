aa47da9362f864d98a17119d5334347f1c2f59b1
nichrosia <nichrosian@gmail.com>
2021-08-16 15:15:32 -0500

    Make NoBreak function correctly again

--- src/main/kotlin/nichrosia/nobreak/content/NBEvents.kt
+++ src/main/kotlin/nichrosia/nobreak/content/NBEvents.kt
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.event.player.AttackEntityCallback
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
 import net.minecraft.client.MinecraftClient
+import net.minecraft.item.ItemStack
 import net.minecraft.item.ShieldItem
 import net.minecraft.text.TranslatableText
 import net.minecraft.util.ActionResult
@@ -19,6 +20,7 @@ object NBEvents : Content {
         PlayerBlockBreakEvents.BEFORE.register Before@ { _, playerEntity, _, _, _ ->
             val stack = playerEntity.getStackInHand(playerEntity.activeHand)
 
+            if (NBSettings.shouldSucceed(stack)) return@Before true
             if (NBSettings.breakingAllowedFor(stack, playerEntity)) return@Before true
 
             playerEntity.inform(
@@ -34,7 +36,7 @@ object NBEvents : Content {
         AttackEntityCallback.EVENT.register Before@ { player, _, hand, _, _ ->
             val stack = player.getStackInHand(hand)
 
-            if (stack.isEmpty) return@Before ActionResult.PASS
+            if (NBSettings.shouldSucceed(stack)) return@Before ActionResult.SUCCESS
             if (NBSettings.breakingAllowedFor(stack, player)) return@Before ActionResult.SUCCESS
 
             player.inform(TranslatableText("text.nobreak.tool_break_prevented", NBKeyBinds.toggleCurrentItemBlacklist.boundKeyLocalizedText))
@@ -45,7 +47,7 @@ object NBEvents : Content {
         UseBlockCallback.EVENT.register Before@ { player, _, hand, _ ->
             val stack = player.getStackInHand(hand)
 
-            if (stack.isEmpty) return@Before ActionResult.PASS
+            if (NBSettings.shouldSucceed(stack)) return@Before ActionResult.SUCCESS
             if (NBSettings.breakingAllowedFor(stack, player)) return@Before ActionResult.PASS
 
             player.inform(TranslatableText("text.nobreak.tool_break_prevented", NBKeyBinds.toggleCurrentItemBlacklist.boundKeyLocalizedText))
@@ -55,38 +57,34 @@ object NBEvents : Content {
 
         ClientTickEvents.END_CLIENT_TICK.register {
             it.player?.let tick@{ player ->
-                run {
-                    if (NBKeyBinds.toggleCurrentItemBlacklist.wasPressed()) {
-                        player.mainHandStack.item.let { item ->
-                            if (!item.isDamageable) return@tick
+                if (NBKeyBinds.toggleCurrentItemBlacklist.wasPressed()) {
+                    player.mainHandStack.item.let { item ->
+                        if (!item.isDamageable) return@tick
 
-                            if (item is ShieldItem) {
-                                player.inform(TranslatableText("text.nobreak.shields_not_supported"))
+                        if (item is ShieldItem) {
+                            player.inform(TranslatableText("text.nobreak.shields_not_supported"))
 
-                                return@tick
-                            }
-
-                            val currentBlacklist = NBSettings.blacklist.copy()
-
-                            NBSettings.blacklist = if (NBSettings.blacklist.items(player).contains(item)) {
-                                 NBSettings.blacklist.copy(items = { currentBlacklist.items(player) - item })
-                            } else {
-                                NBSettings.blacklist.copy(items = { currentBlacklist.items(player) + item })
-                            }
+                            return@tick
                         }
 
-                        player.inform(TranslatableText(
-                            "text.nobreak.toggled_item_blacklist",
-                            onOrOff(NBSettings.blacklist.items(player).contains(player.mainHandStack.item)),
-                        player.mainHandStack.item.name))
+                        val currentBlacklist = NBSettings.blacklist.copy()
+
+                        NBSettings.blacklist = if (NBSettings.blacklist.items(player).contains(item)) {
+                             NBSettings.blacklist.copy(items = { currentBlacklist.items(player) - item })
+                        } else {
+                            NBSettings.blacklist.copy(items = { currentBlacklist.items(player) + item })
+                        }
                     }
+
+                    player.inform(TranslatableText(
+                        "text.nobreak.toggled_item_blacklist",
+                        onOrOff(NBSettings.blacklist.items(player).contains(player.mainHandStack.item)),
+                    player.mainHandStack.item.name))
                 }
             }
 
-            run {
-                if (NBKeyBinds.openBlacklistScreen.wasPressed()) {
-                    MinecraftClient.getInstance().setScreen(BlacklistScreen())
-                }
+            if (NBKeyBinds.openBlacklistScreen.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(BlacklistScreen())
             }
         }
 
--- src/main/kotlin/nichrosia/nobreak/content/NBSettings.kt
+++ src/main/kotlin/nichrosia/nobreak/content/NBSettings.kt
@@ -21,9 +21,28 @@ object NBSettings {
     var notifyUser = true
 
     fun breakingAllowedFor(itemStack: ItemStack, player: PlayerEntity): Boolean {
+        println("Item blacklist contains item: ${blacklist.items(player).contains(itemStack.item)}")
+        println("Breaking allowed for item stack: ${blacklist.isBreakingAllowed(itemStack)}")
+        println("ItemStack not damageable: ${!itemStack.isDamageable}")
+        println("Enchantment predicate: ${(if (itemStack.hasEnchantments()) blacklist.isEnchanted(itemStack) else false)}")
+        println("Total: ${blacklist.items(player).contains(itemStack.item)} | ${blacklist.isBreakingAllowed(itemStack)} | ${!itemStack.isDamageable} | ${(if (itemStack.hasEnchantments()) blacklist.isEnchanted(itemStack) else false)} = ${(blacklist.items(player).contains(itemStack.item) ||
+                blacklist.isBreakingAllowed(itemStack)) ||
+                (if (itemStack.hasEnchantments()) blacklist.isEnchanted(itemStack) else false)}\n\n")
+
         return (blacklist.items(player).contains(itemStack.item) ||
-                blacklist.isBreakingAllowed(itemStack)) &&
-                if (itemStack.hasEnchantments()) blacklist.isEnchanted(itemStack) else true
+                blacklist.isBreakingAllowed(itemStack)) ||
+                !itemStack.isDamageable ||
+                (if (itemStack.hasEnchantments()) blacklist.isEnchanted(itemStack) else false)
+
+    }
+
+    fun shouldSucceed(stack: ItemStack): Boolean {
+        println("Stack empty: ${stack.isEmpty}")
+        println("Stack damageable: ${stack.isDamageable}")
+        println("Stack damage: ${stack.maxDamage - stack.damage}/${stack.maxDamage}")
+        println("Total: ${stack.isEmpty} | ${stack.isDamageable} & ${stack.maxDamage - stack.damage != 1} = ${stack.isEmpty || stack.isDamageable && ((stack.maxDamage - stack.damage) != 1)}\n")
+
+        return stack.isEmpty || stack.isDamageable && ((stack.maxDamage - stack.damage) != 1)
     }
 
     private fun createFiles() {
